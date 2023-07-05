1088854733c9688bc23290e902e22fe2b5011e72
Mariya <mariya.gvar22@gmail.com>
2023-07-05 10:48:37 +0530

    Changes

--- src/App.js
+++ src/App.js
@@ -14,8 +14,7 @@ function App() {
      <Routes>
        <Route path="/" element={<Home/>}/>
        <Route path="/project" element={<Project/>}/>
-       <Route path="/about" element={<About/>}/>
-
+       <Route path="/about" element={<About/>}/> 
      </Routes>
     </>
   );
